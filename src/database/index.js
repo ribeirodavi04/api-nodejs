1a4ae05c8054c8b3de61a92c7cf64a2388ca051d
Davi Ribeiro <ribeirodavi04@outlook.com>
2021-08-12 21:54:21 -0300

    banco de dados e tabela de 'usuarios' criados através do mongodb

---
+++ src/database/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/noderest', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+module.exports = mongoose;
